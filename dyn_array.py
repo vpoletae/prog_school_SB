ef7940f1afcb429601df699492a68b2ce28528e8
Vadim <vadim@poletaev.io>
2019-10-31 22:20:13 +0300

    dyn_array

---
+++ dyn_array.py
@@ -0,0 +1,100 @@
+import ctypes
+
+class DynArray:
+
+    def __init__(self):
+        self.count = 0
+        self.capacity = 16
+        self.array = self.make_array(self.capacity)
+
+    def __len__(self):
+        return self.count
+
+    def make_array(self, new_capacity):
+        return (new_capacity * ctypes.py_object)()
+
+    def __getitem__(self,i):
+        if i < 0 or i > self.count: #change
+            raise IndexError('Index is out of bounds')
+        return self.array[i]
+
+    def resize(self, new_capacity):
+        new_array = self.make_array(new_capacity)
+        for i in range(self.count):
+            new_array[i] = self.array[i]
+        self.array = new_array
+        self.capacity = new_capacity
+
+    def append(self, itm):
+        if self.count == self.capacity:
+            self.resize(2*self.capacity)
+        self.array[self.count] = itm
+        self.count += 1
+
+    def insert(self, i, itm):
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+        elif i == self.count-1:
+            if self.count == self.capacity:
+                self.resize(2*self.capacity)
+                replaced_itm = self.__getitem__(i)
+                self.array[i] = itm
+                self.count += 1
+                self.array[self.count-1] = replaced_itm
+            elif self.count < self.capacity:
+                replaced_itm = self.__getitem__(i)
+                self.array[i] = itm
+                self.count += 1
+                self.array[self.count-1] = replaced_itm
+        else:
+            if self.count == self.capacity:
+                self.resize(2*self.capacity)
+                new_array = self.make_array(self.capacity)
+                for counter in range(i):
+                    new_array[counter] = self.__getitem__(counter)
+                for counter in range(i, self.count):
+                    new_array[counter+1] = self.__getitem__(counter)
+                new_array[i] = itm
+                self.array = new_array
+                self.count += 1
+            elif self.count < self.capacity:
+                new_array = self.make_array(self.capacity)
+                for counter in range(i):
+                    new_array[counter] = self.__getitem__(counter)
+                for counter in range(i, self.count):
+                    new_array[counter+1] = self.__getitem__(counter)
+                new_array[i] = itm
+                self.array = new_array
+                self.count += 1
+
+    def delete(self, i):
+        if i < 0 or i > self.count:
+            raise IndexError('Index is out of bounds')
+        elif i == self.count-1:
+            self.count -= 1
+            if self.count < self.capacity * 0.5:
+                self.resize(int(self.capacity / 1.5))
+                if self.capacity < 16:
+                    self.capacity = 16
+                else:
+                    pass
+            else:
+                pass
+            self.resize(self.capacity)
+        else:
+            new_array = self.make_array(self.capacity)
+            for counter in range(i):
+                new_array[counter] = self.__getitem__(counter)
+            for counter in range(i+1, self.count):
+                new_array[counter-1] = self.__getitem__(counter)
+            self.array = new_array
+            self.count -= 1
+            if self.count < self.capacity * 0.5:
+                self.resize(int(self.capacity / 1.5))
+                if self.capacity < 16:
+                    self.capacity = 16
+                else:
+                    pass
+            else:
+                pass
+            self.resize(self.capacity)
---
+++ dyn_array_test.py
@@ -0,0 +1,243 @@
+import unittest
+from dyn_array import DynArray
+
+class Test_Linked_list(unittest.TestCase):
+
+    def test_insert_out_of_range_before(self):
+        da = DynArray()
+        for i in range(1, 11):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        raised = False
+        try:
+            da.insert(-1, 1000)
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_insert_out_of_range_after(self):
+        da = DynArray()
+        for i in range(1, 11):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        raised = False
+        try:
+            da.insert(100, 1000)
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_insert_last_cap_in(self):
+        da = DynArray()
+        for i in range(10):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.insert(9, 1000)
+        self.assertEqual(da[9], 1000)
+        self.assertEqual(da[8], 8)
+        self.assertEqual(da[10], 9)
+        self.assertEqual(prev_count+1, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_insert_last_cap_out(self):
+        da = DynArray()
+        for i in range(1, 17):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.insert(15, 1000)
+        self.assertEqual(da[15], 1000)
+        self.assertEqual(da[16], 16)
+        self.assertEqual(da[14], 15)
+        self.assertEqual(prev_count+1, da.count)
+        self.assertEqual(prev_capacity*2, da.capacity)
+
+    def test_insert_cap_in(self):
+        da = DynArray()
+        for i in range(10):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.insert(0, 1000)
+        self.assertEqual(da[0], 1000)
+        self.assertEqual(da[1], 0)
+        self.assertEqual(da[10], 9)
+        self.assertEqual(prev_count+1, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_insert_cap_out(self):
+        da = DynArray()
+        for i in range(16):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.insert(0, 1000)
+        self.assertEqual(da[0], 1000)
+        self.assertEqual(da[1], 0)
+        self.assertEqual(da[16], 15)
+        self.assertEqual(prev_count+1, da.count)
+        self.assertEqual(prev_capacity*2, da.capacity)
+
+    def test_delete_out_of_range_before(self):
+        da = DynArray()
+        for i in range(1, 11):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        raised = False
+        try:
+            da.delete(-1, 1000)
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_out_of_range_after(self):
+        da = DynArray()
+        for i in range(1, 11):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        raised = False
+        try:
+            da.delete(100, 1000)
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_last_cap_in_no_resize(self):
+        da = DynArray()
+        for i in range(10):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(9)
+        raised = False
+        try:
+            da[9]
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+1)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_last_cap_in_is_resize_threshold(self):
+        da = DynArray()
+        for i in range(17):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(16)
+        raised = False
+        try:
+            da[16]
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+1)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_last_cap_in_is_resize(self):
+        da = DynArray()
+        for i in range(17):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(16)
+        da.delete(15)
+        raised = False
+        try:
+            da[15]
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+2)
+        self.assertEqual(int(prev_capacity/1.5), da.capacity)
+
+    def test_delete_last_cap_out(self):
+        da = DynArray()
+        for i in range(17):
+            da.append(i)
+        da.capacity = 1000
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(16)
+        raised = False
+        try:
+            da[16]
+        except:
+            raised = True
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+1)
+        self.assertEqual(int(prev_capacity/1.5), da.capacity)
+
+    def test_delete_first_cap_in(self):
+        da = DynArray()
+        for i in range(10):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(0)
+        raised = False
+        try:
+            da[9]
+        except:
+            raised = True
+        self.assertEqual(da[0], 1)
+        self.assertEqual(da[8], 9)
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+1)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_random_cap_in(self):
+        da = DynArray()
+        for i in range(10):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(5)
+        raised = False
+        try:
+            da[9]
+        except:
+            raised = True
+        self.assertEqual(da[4], 4)
+        self.assertEqual(da[5], 6)
+        self.assertEqual(da[8], 9)
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+1)
+        self.assertEqual(prev_capacity, da.capacity)
+
+    def test_delete_cap_out(self):
+        da = DynArray()
+        for i in range(17):
+            da.append(i)
+        prev_count = da.count
+        prev_capacity = da.capacity
+        da.delete(0)
+        da.delete(10)
+        raised = False
+        try:
+            da[15]
+        except:
+            raised = True
+        self.assertEqual(da[0], 1)
+        self.assertEqual(da[10], 12)
+        self.assertEqual(da[9], 10)
+        self.assertEqual(da[11], 13)
+        self.assertEqual(raised, True)
+        self.assertEqual(prev_count, da.count+2)
+        self.assertEqual(int(prev_capacity/1.5), da.capacity)
+
+if __name__ == '__main__':
+    unittest.main()
