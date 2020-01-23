3a9c31291b5c0eb6af13846b55680cb9610b41c9
Vadim Poletaev <43249448+vpoletae@users.noreply.github.com>
2020-01-23 13:50:08 +0300

    hash

---
+++ Hash/Hash.py
@@ -0,0 +1,54 @@
+class HashTable:
+    def __init__(self, sz, stp):
+        self.size = sz
+        self.step = stp
+        self.slots = [None] * self.size
+
+    def hash_fun(self, value):
+        # в качестве value поступают строки!
+        # всегда возвращает корректный индекс слота
+        hash_value = int()
+        for i in range(len(value)):
+            hash_value += ord(value[i])*(i+1)
+        return hash_value % self.size
+
+    def seek_slot(self, value):
+        # находит индекс пустого слота для значения, или None
+        hash_value = self.hash_fun(value)
+        slot = hash_value
+        if self.slots[slot] == None or self.slots[slot] == value:
+            return slot
+        else:
+            counter = int()
+            while not self.slots[slot] == None or \
+                self.slots[slot] == value:
+                slot += self.step
+                if slot >= self.size:
+                    slot -= self.size
+                if counter > self.size:
+                    return None
+                counter += 1
+            return slot
+
+    def put(self, value):
+        # записываем значение по хэш-функции
+        # возвращается индекс слота или None,
+        # если из-за коллизий элемент не удаётся
+        # разместить
+        slot = self.seek_slot(value)
+        if slot != None:
+            self.slots[slot] = value
+            return slot
+        else:
+            return None
+
+    def find(self, value):
+        # находит индекс слота со значением, или None
+        slot = self.seek_slot(value)
+        if slot != None:
+            if self.slots[slot] == value:
+                return slot
+            else:
+                return None
+        else:
+            return None
---
+++ Hash/Hash_test.py
@@ -0,0 +1,39 @@
+import unittest
+from Hash import HashTable
+
+class Test_Hash(unittest.TestCase):
+
+    def test_hash_fun(self):
+        hash_table = HashTable(11, 3)
+        value = 'cat'
+        hash_value = hash_table.hash_fun(value)
+        hash_value_tested = int()
+        for i in range(len(value)):
+            hash_value_tested += ord(value[i])*(i+1)
+        self.assertEqual(hash_value, (hash_value_tested % hash_table.size))
+        self.assertNotEqual(hash_value, hash_table.hash_fun('tac'))
+
+    def test_seek_slot(self):
+        hash_table = HashTable(5, 3)
+        s1 = hash_table.seek_slot('a')
+        s2 = hash_table.seek_slot('b')
+        s3 = hash_table.seek_slot('c')
+        s4 = hash_table.seek_slot('d')
+        s5 = hash_table.seek_slot('e')
+        s6 = hash_table.seek_slot('f')
+        self.assertEqual(s1 != s2 != s3 != s4 != s5, True)
+        self.assertEqual(s6 in [s1, s2, s3, s4, s5], True)
+
+    def test_put_find(self):
+        hash_table = HashTable(5, 3)
+        s1 = hash_table.put('a')
+        s2 = hash_table.put('b')
+        s3 = hash_table.put('c')
+        s4 = hash_table.put('d')
+        s5 = hash_table.put('e')
+        s6 = hash_table.put('f')
+        self.assertEqual(hash_table.find('a') != hash_table.find('b') != hash_table.find('c') != hash_table.find('d')  != hash_table.find('e'), True)
+        self.assertEqual(hash_table.find('f'), None)
+
+if __name__ == '__main__':
+    unittest.main()
