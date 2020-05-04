d74e1e8df21264116a24a68a4f038ffafa2d8456
Vadim Poletaev <43249448+vpoletae@users.noreply.github.com>
2020-05-04 13:50:44 +0300

    Balanced_BST

---
+++ Balanced_BST/Balanced_BST.py
@@ -0,0 +1,42 @@
+import math
+
+def GenerateBBSTArray(a):
+    depth = int(math.log((len(a) + 1), 2) - 1)
+    if len(a) == 0:
+        return []
+    elif len(a) == 1:
+        return a
+    else:
+        ordered_array = order_array(sorted(a))
+        if len(ordered_array) <= 3:
+            return ordered_array
+        else:
+            reordered = reorder(ordered_array, depth)
+            return reordered
+
+def reorder(ordered_array, depth):
+    to_reorder = ordered_array[1:]
+    mid_index = int(len(to_reorder) / 2 - 1)
+    left_subtree = to_reorder[:(mid_index + 1)]
+    right_subtree = to_reorder[(mid_index + 1):]
+    reordered = ordered_array[:1]
+    for level in range(depth):
+        nodes_on_lvl = 2 ** level
+        for i in range(nodes_on_lvl):
+            to_add = left_subtree.pop(0)
+            reordered += [to_add]
+        for i in range(nodes_on_lvl):
+            to_add = right_subtree.pop(0)
+            reordered += [to_add]
+    return reordered
+
+
+def order_array(sorted_array):
+    if len(sorted_array) <= 2:
+        return sorted_array
+    index = int()
+    if len(sorted_array) % 2 == 0:
+        index = (len(sorted_array) // 2) - 1
+    else:
+        index = (len(sorted_array) // 2 + 1) - 1
+    return [sorted_array[index]] + order_array(sorted_array[:index]) + order_array(sorted_array[(index + 1):])
---
+++ Balanced_BST/Balanced_BST_test.py
@@ -0,0 +1,27 @@
+import unittest
+from Balanced_BST import *
+
+class Test_Balanced_BST(unittest.TestCase):
+
+    def test_empty_array(self):
+        array = []
+        self.assertEqual(GenerateBBSTArray(array), [])
+
+    def test_unit_array(self):
+        array = [1]
+        self.assertEqual(GenerateBBSTArray(array), [1])
+
+    def test_double_array(self):
+        array = [2,1]
+        self.assertEqual(GenerateBBSTArray(array), [1,2])
+
+    def test_triple_array(self):
+        array = [3,2,1]
+        self.assertEqual(GenerateBBSTArray(array), [2,1,3])
+
+    def test_7_array(self):
+        array = [3,2,1,4,5,6,7]
+        self.assertEqual(GenerateBBSTArray(array), [4,2,6,1,3,5,7])
+
+if __name__ == '__main__':
+    unittest.main()
