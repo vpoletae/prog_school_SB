b331ce2843a011b50fdff171d9c29704a5c742ac
Vadim Poletaev <43249448+vpoletae@users.noreply.github.com>
2020-05-10 16:28:13 +0300

    Balanced_BST

--- Balanced_BST/Balanced_BST.py
+++ Balanced_BST/Balanced_BST.py
@@ -1,43 +1,25 @@
-# import math
-
 def GenerateBBSTArray(a):
-    # depth = int(math.log((len(a) + 1), 2) - 1)
-    if len(a) == 0:
+    if a == []:
         return []
-    elif len(a) == 1:
-        return a
+    a_sorted = sort_array(a)
+    empty = create_empty_array(a)
+    balanced = fulfill_empty(a_sorted, empty, index=0)
+    return balanced
+
+def sort_array(a):
+    return sorted(a)
+
+def create_empty_array(a):
+    return [None] * len(a)
+
+def fulfill_empty(a, empty, index=0):
+    if a == []:
+        pass
     else:
-        ordered_array = order_array(sorted(a))
-        return(ordered_array)
-        # if len(ordered_array) <= 3:
-        #     return ordered_array
-        # else:
-        #     reordered = reorder(ordered_array, depth)
-        #     return reordered
-
-# def reorder(ordered_array, depth):
-#     to_reorder = ordered_array[1:]
-#     mid_index = int(len(to_reorder) / 2 - 1)
-#     left_subtree = to_reorder[:(mid_index + 1)]
-#     right_subtree = to_reorder[(mid_index + 1):]
-#     reordered = ordered_array[:1]
-#     for level in range(depth):
-#         nodes_on_lvl = 2 ** level
-#         for i in range(nodes_on_lvl):
-#             to_add = left_subtree.pop(0)
-#             reordered += [to_add]
-#         for i in range(nodes_on_lvl):
-#             to_add = right_subtree.pop(0)
-#             reordered += [to_add]
-#     return reordered
-
-
-def order_array(sorted_array):
-    if len(sorted_array) <= 2:
-        return sorted_array
-    index = int()
-    if len(sorted_array) % 2 == 0:
-        index = (len(sorted_array) // 2) - 1
-    else:
-        index = (len(sorted_array) // 2 + 1) - 1
-    return [sorted_array[index]] + order_array(sorted_array[:index]) + order_array(sorted_array[(index + 1):])
+        mid_index = int(len(a) / 2)
+        empty[index] = a[mid_index]
+        left_index = 2 * index + 1
+        right_index = 2 * index + 2
+        fulfill_empty(a[:mid_index], empty, left_index)
+        fulfill_empty(a[mid_index + 1:], empty, right_index)
+    return empty
--- Balanced_BST/Balanced_BST_test.py
+++ Balanced_BST/Balanced_BST_test.py
@@ -3,25 +3,25 @@ from Balanced_BST import *
 
 class Test_Balanced_BST(unittest.TestCase):
 
-    def test_empty_array(self):
-        array = []
-        self.assertEqual(GenerateBBSTArray(array), [])
+    # def test_empty_array(self):
+    #     array = []
+    #     self.assertEqual(GenerateBBSTArray(array), [])
 
-    def test_unit_array(self):
-        array = [1]
-        self.assertEqual(GenerateBBSTArray(array), [1])
+    # def test_unit_array(self):
+    #     array = [1]
+    #     self.assertEqual(GenerateBBSTArray(array), [1])
 
-    def test_double_array(self):
-        array = [2,1]
-        self.assertEqual(GenerateBBSTArray(array), [1,2])
+    # def test_triple_array(self):
+    #     array = [3,2,1]
+    #     self.assertEqual(GenerateBBSTArray(array), [2,1,3])
 
-    def test_triple_array(self):
-        array = [3,2,1]
-        self.assertEqual(GenerateBBSTArray(array), [2,1,3])
+    # def test_7_array(self):
+    #     array = [3,2,1,4,5,6,7]
+    #     self.assertEqual(GenerateBBSTArray(array), [4,2,6,1,3,5,7])
 
-    def test_7_array(self):
-        array = [3,2,1,4,5,6,7]
-        self.assertEqual(GenerateBBSTArray(array), [4,2,1,3,6,5,7])
+    def test_15_array(self):
+        array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+        self.assertEqual(GenerateBBSTArray(array), [8,4,12,2,6,10,14,1,3,5,7,9,11,13,15])
 
 if __name__ == '__main__':
     unittest.main()
