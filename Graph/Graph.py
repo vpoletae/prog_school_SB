780c85fd83aab6a355f3908220aefde17f32620f
Vadim Poletaev <43249448+vpoletae@users.noreply.github.com>
2020-05-25 22:36:44 +0300

    dfs

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -2,6 +2,7 @@ class Vertex:
 
     def __init__(self, val):
         self.Value = val
+        self.Hit = False
 
 class SimpleGraph:
 
@@ -57,3 +58,40 @@ class SimpleGraph:
         else:
             self.m_adjacency[v1][v2] = 0
             self.m_adjacency[v2][v1] = 0
+
+    def DepthFirstSearch(self, VFrom, VTo, stack=None, trigger=0):
+        # узлы задаются позициями в списке vertex
+        # возвращается список узлов -- путь из VFrom в VTo
+        # или [] если пути нету
+        if trigger == 0:
+            stack = []
+            for vertex in self.vertex:
+                vertex.Hit = False
+            trigger = 1
+        vertex = self.vertex[VFrom]
+        vertex.Hit = True
+        if vertex not in stack:
+            stack.append(vertex)
+        adj_list = self.m_adjacency[VFrom]
+        if adj_list[VTo] == 1:
+            stack.append(self.vertex[VTo])
+        else:
+            # get list of adjacent vertex indeces
+            adj_vertex = []
+            for i in range(len(adj_list)):
+                if adj_list[i] == 1:
+                    if self.vertex[i].Hit == False:
+                        adj_vertex.append(i)
+                else:
+                    pass
+            if adj_vertex != []:
+                for index in adj_vertex:
+                    self.DepthFirstSearch(index, VTo, stack, trigger)
+            else:
+                stack.pop(-1)
+                if stack == []:
+                    pass
+                else:
+                    index = self.vertex.index(stack[-1])
+                    self.DepthFirstSearch(index, VTo, stack, trigger)
+        return stack
