23c2821925b40c1b478b3f39d877a47e10c49826
Vadim Poletaev <43249448+vpoletae@users.noreply.github.com>
2020-06-12 14:04:58 +0300

    insertion_step

--- Sorting/sorting_l1.py
+++ Sorting/sorting_l1.py
@@ -31,3 +31,22 @@ def BubbleSortStep(array: list)-> bool:
         return True
     else:
         return False
+
+def InsertionSortStep(array: list, step: int, index: int):
+
+    if len(array) <= 1:
+        pass
+    else:
+        next_index = index + step
+        if next_index > len(array)-1:
+            return array
+        else:
+            indexes = [index]
+            values = [array[index]]
+            while not next_index > len(array)-1:
+                indexes.append(next_index)
+                values.append(array[next_index])
+                next_index += step
+        index_value = list(zip(indexes, sorted(values)))
+        for pair in index_value:
+            array[pair[0]] = pair[1]
--- Sorting/sorting_l1_test.py
+++ Sorting/sorting_l1_test.py
@@ -3,40 +3,40 @@ import unittest
 
 class Test_sorting(unittest.TestCase):
 
-    def test_selection(self):
-        array = [4,3,1,2]
-        SelectionSortStep(array, 0)
-        self.assertEqual(array, [1, 3, 4, 2])
-        # base
-        array = [4,3,1,2]
-        for i in range(len(array)):
-            SelectionSortStep(array, i)
-        self.assertEqual(array, [1, 2, 3, 4])
-        self.assertEqual(len(array), 4)
-        # empty
-        array = []
-        for i in range(len(array)):
-            SelectionSortStep(array, i)
-        self.assertEqual(array, [])
-        self.assertEqual(len(array), 0)
-        # Unit
-        array = [1]
-        for i in range(len(array)):
-            SelectionSortStep(array, i)
-        self.assertEqual(array, [1])
-        self.assertEqual(len(array), 1)
-        # two
-        array = [2, 1]
-        for i in range(len(array)):
-            SelectionSortStep(array, i)
-        self.assertEqual(array, [1, 2])
-        self.assertEqual(len(array), 2)
-        # two sorted
-        array = [1, 2, 3, 4]
-        for i in range(len(array)):
-            SelectionSortStep(array, i)
-        self.assertEqual(array, [1, 2, 3, 4])
-        self.assertEqual(len(array), 4)
+    # def test_selection(self):
+    #     array = [4,3,1,2]
+    #     SelectionSortStep(array, 0)
+    #     self.assertEqual(array, [1, 3, 4, 2])
+    #     # base
+    #     array = [4,3,1,2]
+    #     for i in range(len(array)):
+    #         SelectionSortStep(array, i)
+    #     self.assertEqual(array, [1, 2, 3, 4])
+    #     self.assertEqual(len(array), 4)
+    #     # empty
+    #     array = []
+    #     for i in range(len(array)):
+    #         SelectionSortStep(array, i)
+    #     self.assertEqual(array, [])
+    #     self.assertEqual(len(array), 0)
+    #     # Unit
+    #     array = [1]
+    #     for i in range(len(array)):
+    #         SelectionSortStep(array, i)
+    #     self.assertEqual(array, [1])
+    #     self.assertEqual(len(array), 1)
+    #     # two
+    #     array = [2, 1]
+    #     for i in range(len(array)):
+    #         SelectionSortStep(array, i)
+    #     self.assertEqual(array, [1, 2])
+    #     self.assertEqual(len(array), 2)
+    #     # two sorted
+    #     array = [1, 2, 3, 4]
+    #     for i in range(len(array)):
+    #         SelectionSortStep(array, i)
+    #     self.assertEqual(array, [1, 2, 3, 4])
+    #     self.assertEqual(len(array), 4)
 
     # def test_bubble(self):
     #     array = [4,3,1,2]
@@ -57,6 +57,28 @@ class Test_sorting(unittest.TestCase):
     #     array = [1, 3]
     #     self.assertEqual(BubbleSortStep(array), True)
     #     self.assertEqual(array, [1, 3])
+
+    def test_insertion_step(self):
+        array = [1,6,5,4,3,2,7]
+        InsertionSortStep(array, 3, 1 )
+        self.assertEqual(array, [1,3,5,4,6,2,7])
+        array = [1,6,5,4,3,2,7]
+        InsertionSortStep(array, 1, 0 )
+        self.assertEqual(array, [1,2,3,4,5,6,7])
+        array = [1,6,5,4,3,2,7]
+        InsertionSortStep(array, 3, 6 )
+        self.assertEqual(array, [1,6,5,4,3,2,7])
+        array = [1,6,5,4,3,2,7]
+        InsertionSortStep(array, 2, 1 )
+        self.assertEqual(array, [1,2,5,4,3,6,7])
+        # empty
+        array = []
+        InsertionSortStep(array, 2, 1 )
+        self.assertEqual(array, [])
+        # unit
+        array = [1]
+        InsertionSortStep(array, 2, 1 )
+        self.assertEqual(array, [1])
 
 if __name__ == '__main__':
     unittest.main()
